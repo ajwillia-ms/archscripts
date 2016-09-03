1582b6d81063ed9ec34d75b50007beba9d0cf4a6
Andy Williams <andy@andywilliams.me>
2016-09-03 16:26:20 +0100

    Remove all output from screen
    Store in a log file that can be refferenced if there is an error

--- e_build.sh
+++ e_build.sh
@@ -6,22 +6,38 @@ REPO_URL="https://git.enlightenment.org"
 
 REPO_LIST="core/efl core/enlightenment apps/equate apps/terminology tools/edi"
 
+LOG_FILE=.e_build.out
+
+function quit {
+  IFS='
+'
+  echo "[FAIL ] Exiting script - last 5 lines of output (see .e_build.out)"
+  echo
+
+  if [ -e "$LOG_FILE" ]; then
+    for LINE in `tail -5 .e_build.out`; do
+      echo "[LOG  ] $LINE";
+    done
+  fi
+  exit 1
+}
+
 for REPO in $REPO_LIST ; do
   NAME=`echo $REPO|cut -d'/' -f2`
   if [ -d "$NAME" ]; then
     echo "[PULL ] ${NAME}"
     cd ${NAME}
-    git pull
+    git pull -q --rebase 1>&2 2>$LOG_FILE || quit
     cd ..
   else
     echo "[CLONE] ${NAME}"
-    git clone ${REPO_URL}/${REPO}.git ${NAME}
+    git clone -q ${REPO_URL}/${REPO}.git ${NAME} 1>&2 2>$LOG_FILE || quit
   fi
 
   echo "[BUILD] ${NAME}"
   cd ${NAME}
-  ./autogen.sh
-  make
+  ./autogen.sh > /dev/null 2>$LOG_FILE || quit
+  make > /dev/null 2>$LOG_FILE || quit
   echo "[INST ] ${NAME}"
   sudo make install
   cd ..
